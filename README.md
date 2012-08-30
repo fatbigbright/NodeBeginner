d69d35f5e01e4741e1d14411d24d4d6e40f0be2b
Chang, Liang <bigbrightf@gmail.com>
2012-08-30 14:36:20 +0800

    entry point - index.js

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ NodeBeginner
 following http://www.nodebeginner.org step by step to build a simple pic-upload web site.
 
 08/30/2012 - Initialized.
+
+             Create index.js as Entrypoint, wrap code in server.js 
+             and make it as a module to export a function - start();
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require("http");
+function start(){
+    http.createServer(function(request, response){
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write("Hello World");
+        response.end();
+    }).listen(8888);
 
-http.createServer(function(request, response){
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+
+    console.log("Server Started.");
+}
+
+exports.start = start;
